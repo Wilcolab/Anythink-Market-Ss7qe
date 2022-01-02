0a5653db11235122af2c48daaa4558be8fed1c59
Lior Brauer <lior.brauer@forter.com>
2022-01-02 14:47:51 +0200

    seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create! email: 'user12@test.com', password: Random.hex, username: 'user12'
+item1 = Item.create! title: 'test item 1', description: 'amazing', user: user
+item2 = Item.create! title: 'test item 2', description: 'amazing', user: user
+item3 = Item.create! title: 'test item 3', description: 'amazing', user: user
